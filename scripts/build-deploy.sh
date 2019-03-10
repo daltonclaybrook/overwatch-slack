a6c14de8cbccac75a0aad67438e86da6fabea900
Dalton Claybrook <daltonclaybrook@gmail.com>
2019-03-10 18:50:45 -0400

    Now specifying the task definition argument of aws ecs update-service so that it will always use the latest task definition revision

--- scripts/build-deploy.sh
+++ scripts/build-deploy.sh
@@ -4,4 +4,4 @@ $(aws ecr get-login --no-include-email --region us-east-1)
 docker build --build-arg env=docker -t overwatch .
 docker tag overwatch:latest 073174792857.dkr.ecr.us-east-1.amazonaws.com/overwatch:latest
 docker push 073174792857.dkr.ecr.us-east-1.amazonaws.com/overwatch:latest
-aws ecs update-service --cluster overwatch --service overwatch-service --force-new-deployment
+aws ecs update-service --cluster overwatch --service overwatch-service --task-definition overwatch --force-new-deployment
